6e76f393348fa2d3adcd09374ad017cde4deba60
elnegrochoque <75055678+elnegrochoque@users.noreply.github.com>
2021-07-23 20:58:41 -0300

    se configuro el backend y se hizo la coneccion a la BD

---
+++ src/database.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const url='mongodb://localhost:27017/sistemadeevaluaciones'
+
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+
+const connection =mongoose.connection;
+connection.once('open', ()=>{
+    console.log('BD conectada')
+})
