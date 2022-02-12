ac2ab7ed294eb0220ce72595c66a54e7b6361ff4
elnegrochoque <elnegrochoque@gmail.com>
2022-02-12 15:31:29 -0300

    -

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
-//const url='mongodb://localhost:27017/sistemadeevaluaciones'
-const url="mongodb+srv://elnegrochoque:elnegrochoque_11@cluster0.yy3fo.mongodb.net/proyecto4"
+const url='mongodb://localhost:27017/sistemadeevaluaciones'
+//const url="mongodb+srv://elnegrochoque:elnegrochoque_11@cluster0.yy3fo.mongodb.net/proyecto4"
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
 
 const connection =mongoose.connection;
